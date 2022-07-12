d682819c3e534c00591aff5eac489ceaddf97132
Derryck <derryckadu@gmail.com>
2022-07-12 00:29:31 -0400

    Adds the schema for how a win is determined
    Add the aWin function that uses a ternary operator to run through
    a possibility of wins to figure out the winner of a Rock Paper Scissor
    Game

--- Javascript.js
+++ Javascript.js
@@ -10,6 +10,35 @@ function computerPlay() {
     )
 }
 
+function playerPlay() { 
+
+}
+
+function gameofRockPaperScissors(playerSelection, ComputerSelection) {
+
+}
+
+function winSchema(a, b) {
+    //Rock POV
+    a === "Rock" && b === "Scissors" ? "Rock beats Scissors, You Win!"
+    : a === "Rock" && b === "Paper" ? "Rock loses to  Paper, You Lose!"
+    : a === "Rock" && b === "Rock" ? "Rock can't beat Rock, it's a Tie!"
+    //Paper POV
+    : a === "Paper" && b === "Scissors" ? "Paper loses Scissors, You Lose!"
+    : a === "Paper" && b === "Rock" ? "Paper beats Rock, You Win!"
+    : a === "Paper" && b === "Paper" ? "Paper can't beat Paper, it's a Tie!"
+    //Scissors POV
+    : a === "Scissors" && b === "Scissors" ? "Scissors cant beat Scissors, it's a tie!"
+    : a === "Scissors" && b === "Rock" ? "Scissors loses to Rock, You Lose!"
+    : a === "Scissors" && b === "Paper" ? "Scissors beats Paper, its a Tie!"
+
+}
+
+
+
+
+
+
 console.log(computerPlay());
 console.log(computerPlay());
 console.log(computerPlay());
