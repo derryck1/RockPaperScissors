05c2e95ae3147070f6601e508b5db4d2b591f02d
Derryck <derryckadu@gmail.com>
2022-07-12 22:54:06 -0400

    Creates Win Schema, detailing how to win RPS
    Creates winSchema function. Essentially runs 9 test cases
    To determine the winner of a rock paper scissors match
    In it's current form it only takes exact string matches.

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,5 @@
-
 //
-function computerPlay() {
+function  computerPlay() {
     let a = Math.floor(Math.random() * 3) + 1;
      
     return (
@@ -10,19 +9,19 @@ function computerPlay() {
     )
 }
 
-function playerPlay() { 
-
-}
-
-function gameofRockPaperScissors(playerSelection, ComputerSelection) {
-
+function gameOfRockPaperScissors(playerSelection, computerSelection) {
+    let playerPlay = prompt("Rock, Paper, or Scissors", "?");
+    playerSelection = playerPlay;
+    computerSelection = computerPlay();
+    return winSchema(playerSelection, computerSelection);
 }
 
 function winSchema(a, b) {
+    return ( 
     //Rock POV
-    a === "Rock" && b === "Scissors" ? "Rock beats Scissors, You Win!"
-    : a === "Rock" && b === "Paper" ? "Rock loses to  Paper, You Lose!"
-    : a === "Rock" && b === "Rock" ? "Rock can't beat Rock, it's a Tie!"
+    a == "Rock" && b == "Scissors" ? "Rock beats Scissors, You Win!"
+    : a == "Rock" && b == "Paper" ? "Rock loses to  Paper, You Lose!"
+    : a == "Rock" && b == "Rock" ? "Rock can't beat Rock, it's a Tie!"
     //Paper POV
     : a === "Paper" && b === "Scissors" ? "Paper loses Scissors, You Lose!"
     : a === "Paper" && b === "Rock" ? "Paper beats Rock, You Win!"
@@ -30,15 +29,10 @@ function winSchema(a, b) {
     //Scissors POV
     : a === "Scissors" && b === "Scissors" ? "Scissors cant beat Scissors, it's a tie!"
     : a === "Scissors" && b === "Rock" ? "Scissors loses to Rock, You Lose!"
-    : a === "Scissors" && b === "Paper" ? "Scissors beats Paper, its a Tie!"
-
+    : a === "Scissors" && b === "Paper" ? "Scissors beats Paper, You Win!!"
+    : "Enter a valid value of either Rock, Paper or Scissors"
+    )    
 }
 
+console.log(gameOfRockPaperScissors());
 
-
-
-
-
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
