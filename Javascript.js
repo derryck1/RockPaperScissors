6a6560ba80157eb6a3447f92931c919daba67bbc
Derryck <derryckadu@gmail.com>
2022-07-14 00:45:03 -0400

    Creates multiple functions to complete RSP game
    I didn't commit as much as I should while making this since I was in the
    moment, but this essentially fleshes out my entire RPS game. I created
    these functions:
    
    function(game) - This function runs 5 rounds of RockPaperScissors.
    Using a for loop it runs a round of Rock Paper Scissors. After each
    round it logs the current score, to the console. After all 5 rounds are
    complete, itderryck@derryck-VirtualBox:~/JavaScript/RockPaperScissors$ git log
    returns an if statement/ternary statement based on whoever
    has the higher score and outputs a message from the perspective of the
    player. The message informs the player of how many rounds were played
    How many of them the player won, how many of them the computer won,
    and how many ties there were
    
    function playRound - nothing has changed here from the last commit
    takes two parameters. Runs the function expression playerPlay
    Then sets the players input to the first parameter and the computers
    randomized choice to the second parameter. It then passes these
    To the winSchema function to compare the two.
    
    function computerPlay() - also not too different from last commit
    Uses math.floor and math.random to generate a random number between
    1 and 3 inclusively. Based on the number recieved it return a starting
    
    function winSchema() this is essentially the map of what a win would
    or a loss would look like for the player depending on the move chosen
    compared to the randomized npcs move. Depending on what you choose it
    returns a count function
    
    count functions - wrote these to keep track of wins and losses to tally
    up the total score at the end. I went a little extra in on these for the
    sake of practice and close to perfection local
    
    tieCount - logs that match was a tie, adds one to tie counter, reruns
    playRound (doesn't count towards the 5 rounds of the loop) also logs
    score again as it adds to the tie counter
    rockCount - logs the match as a win for player, using rock, adds 1 to
    rock counter
    paperCount - logs the match as a win for player, using paper, adds 1 to
    paper counter
    scissorCount - logs the match as a win for player, using scissors, adds 1 to
    scissor counter
    npcRockCount -logs the match as a win for computer, using rock, adds 1 to
    npcrock counter
    npcPaperCount - logs the match as a win for computer, using paper, adds 1 to
    npcpaper counter
    npcScissorCount - logs the match as a win for computer, using scissor, adds 1 to
    npcscissor counter

--- Javascript.js
+++ Javascript.js
@@ -1,38 +1,134 @@
-//
-function  computerPlay() {
-    let a = Math.floor(Math.random() * 3) + 1;
-     
+//Base Values for counting
+let rock = 0;
+let paper = 0;
+let scissors = 0;
+let npcrock = 0;
+let npcpaper = 0;
+let npcscissors = 0;
+let ties = 0;
+let playerScore = 0; 
+let npcScore = 0;
+let totalRounds;
+
+
+//runs 5 Rounds of Rock Paper Scissors and Returns the winner of the game,  the amount of rounds played, 
+//how many games the player won, and how many games the npc won.
+function game() {
+    for (i = 0; i < 5; i++) {
+        playRound()
+        playerScore = rock + paper + scissors
+        npcScore = npcrock + npcpaper + npcscissors
+        totalRounds = npcScore + playerScore + ties;
+        console.log(`Current Score; Player: ${playerScore}, NPC: ${npcScore}, ${ties} Ties`)
+    }
+    
     return (
-        a == 1 ? "Rock"
-        : a == 2 ? "Paper"
-        : "Scissors"
-    )
+        playerScore > npcScore ? `YOU WIN THE ENTIRE GAME. GREAT JOB. There were ${totalRounds} rounds,
+you won ${playerScore} of them, the npc won ${npcScore} of them, and there were ${ties} ties.`
+        
+        : npcScore > playerScore ?`THE NPC WON THE ENTIRE GAME. TOUGH LUCK. There were ${totalRounds} rounds, 
+you won ${playerScore} of them, the npc won ${npcScore} of them, and there were ${ties} ties.`
+    
+        : `Game cancelled. The player didn't enter Rock, Paper or Scissors`
+        )
 }
-//
-function gameOfRockPaperScissors(playerSelection, computerSelection) {
+
+
+//runs a single game of rock paper scissors
+function playRound(playerSelection, computerSelection) {
     let playerPlay = prompt("Rock, Paper, or Scissors", "?");
     playerSelection = playerPlay;
     computerSelection = computerPlay();
     return winSchema(playerSelection, computerSelection);
 }
 
-function winSchema(a, b) {
-    return ( 
-    //Rock POV
-    a == "Rock" && b == "Scissors" ? "Rock beats Scissors, You Win!"
-    : a == "Rock" && b == "Paper" ? "Rock loses to  Paper, You Lose!"
-    : a == "Rock" && b == "Rock" ? "Rock can't beat Rock, it's a Tie!"
-    //Paper POV
-    : a === "Paper" && b === "Scissors" ? "Paper loses Scissors, You Lose!"
-    : a === "Paper" && b === "Rock" ? "Paper beats Rock, You Win!"
-    : a === "Paper" && b === "Paper" ? "Paper can't beat Paper, it's a Tie!"
-    //Scissors POV
-    : a === "Scissors" && b === "Scissors" ? "Scissors cant beat Scissors, it's a tie!"
-    : a === "Scissors" && b === "Rock" ? "Scissors loses to Rock, You Lose!"
-    : a === "Scissors" && b === "Paper" ? "Scissors beats Paper, You Win!!"
-    : "Enter a valid value of either Rock, Paper or Scissors"
-    )    
+//Defines the paramters for the NPC's choices
+// returns a string based on the randomzied number between 1 and 3
+function  computerPlay() {
+    let a = Math.floor(Math.random() * 3) + 1;
+    return (
+        a == 1 ? "Rock"
+        : a == 2 ? "Paper"
+        : a == 3 ? "Scissors" 
+        : "Computer Error"
+    )
 }
 
-console.log(gameOfRockPaperScissors());
+//creates the parameters for what a win looks like through boolean comparisons
 
+function winSchema(a, b) {
+    return ( 
+    //Rock POV        
+    a == "Rock" && b == "Scissors" ? rockCount()
+    : a == "Rock" && b == "Paper" ? npcPaperCount()
+    : a == "Rock" && b == "Rock" ? tieCount() 
+    //Paper POV
+    : a === "Paper" && b === "Scissors" ? npcScissorCount()
+    : a === "Paper" && b === "Rock" ? paperCount()
+    : a === "Paper" && b === "Paper" ? tieCount()
+    //Scissors POV
+    : a === "Scissors" && b === "Scissors" ? tieCount()
+    : a === "Scissors" && b === "Rock" ? npcRockCount()
+    : a === "Scissors" && b === "Paper" ? scissorCount()
+    : invalidRound()
+    )
+}
+
+//counter functions
+
+//Tie counter. Adds to counter when there is a tie, and re-runs the game of Rock Paper Scissors
+function tieCount() {
+    console.log("This round was a tie! Go again!")
+    ties += 1;
+    console.log(`Current Score; Player: ${playerScore}, NPC: ${npcScore}, ${ties} Ties`)
+    playRound();
+    
+}
+
+//player Rock Win Counter
+function rockCount() {
+    console.log("You won this round with Rock! Nice!")
+    rock += 1;
+}
+
+//player Paper Win Counter
+function paperCount() {
+    console.log("You won this round with Paper! Nice!")
+    paper += 1;
+
+}
+
+//player Scissor Win Counter
+function scissorCount() {
+    console.log("You won this round with Scissors! Nice!")
+    scissors += 1;
+
+}
+
+//npc Rock Win Counter
+function npcRockCount() {
+    console.log("The computer won this round with Rock! Damn")
+    npcrock += 1;
+}
+
+//npc Paper Win Counter
+function npcPaperCount() {
+    console.log("The computer won this round with Paper! Damn")
+    npcpaper += 1;
+}
+
+//npc Scissor Win Counter
+function npcScissorCount() {
+    console.log("The computer won this round with Scissors! Damn")
+    npcscissors += 1;
+}
+
+//invalid game function
+
+function invalidRound() {
+    console.log("Enter a valid value of either Rock, Paper or Scissors")
+    playRound();
+    
+}
+
+    console.log(game());
